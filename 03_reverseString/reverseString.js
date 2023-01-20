b9e05cbd16732701427c67e4eb6e11077998c963
Melissa Johnson <melissajohnson326@gmail.com>
2023-01-20 18:30:11 -0500

    Complete exercise to pass all tests in spec.js file

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitString = str.split('');// split string into a new array
+    let reverseString = splitString.reverse();// reverse order of the new array
+    let joinArray = reverseString.join('');// join all parts of reversed array 
+    return joinArray;
+
 
 };
+reverseString('')
 
 // Do not edit below this line
 module.exports = reverseString;
