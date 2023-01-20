6c9ea951820982bca88385f4674da8f84fc8623c
Melissa Johnson <melissajohnson326@gmail.com>
2023-01-20 17:59:12 -0500

    Complete exercise to pass all tests specifice in spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function (hey, num) {
+    let string= ''
+        if (num < 0) 
+        return 'ERROR';
+    else 
+    for( i=0; i<num; i++)
+    string += hey
+     return string;
+}
 
-};
+repeatString()
 
 // Do not edit below this line
 module.exports = repeatString;
