6bdab0c62405ddc610dcbc644bd63663545494eb
Melissa Johnson <melissajohnson326@gmail.com>
2023-01-22 18:18:23 -0500

    Complete exercise so that all tests in spec.js pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array= args[0];// creates an array out of the elements
+    const newArray = []; // create new empty array to put remaining elements
 
-};
-
+    array.forEach((item) => { // goes through each item in array
+        if (!args.includes(item)) { //compares each item in array to the other value
+          newArray.push(item); //if items do not match they are pushed to new array
+        }
+      });
+      return newArray;
+    };
+    
+ 
+ 
 // Do not edit below this line
 module.exports = removeFromArray;
