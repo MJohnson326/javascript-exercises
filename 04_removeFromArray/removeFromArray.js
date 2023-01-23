e638389d44edc52e0ea3adc3f2231f5176eefeed
Melissa Johnson <melissajohnson326@gmail.com>
2023-01-22 19:06:03 -0500

    add another solution  using the .filter function and add comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,22 @@
-const removeFromArray = function(...args) {
-    const array= args[0];// creates an array out of the elements
-    const newArray = []; // create new empty array to put remaining elements
+ const removeFromArray = function(array,...args){ 
+return array.filter(item => //filter through each item in array
+  !args.includes(item)) //item does not equal args array put  in new array
+  }
 
-    array.forEach((item) => { // goes through each item in array
-        if (!args.includes(item)) { //compares each item in array to the other value
-          newArray.push(item); //if items do not match they are pushed to new array
-        }
-      });
-      return newArray;
-    };
-    
- 
- 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/* const removeFromArray = function(...args) {
+  const array= args[0];// creates an array out of the elements
+  const newArray = []; // create new empty array to put remaining elements
+
+  array.forEach((item) => { // goes through each item in array
+     if (!args.includes(item)) { //compares each item in array to the other value
+        newArray.push(item); //if items do not match they are pushed to new array
+      }
+    });
+    return newArray;
+  };
+  
+  */
