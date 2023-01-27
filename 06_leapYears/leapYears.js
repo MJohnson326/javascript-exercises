2640b0db048db8ed37eac4b8fd45857c8017a610
Melissa Johnson <melissajohnson326@gmail.com>
2023-01-26 19:11:19 -0500

    Complete exercise and pass all tests in spec. js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) { //parameter is a year
 
-};
+if ((year %4===0) &&((year % 100 !==0)|| ((year % 400)===0))){// divisible by 4 but non centurn unless divided by 400
+    return true
+}else {
+    return false}
+}
+
+
+ 
 
 // Do not edit below this line
 module.exports = leapYears;
