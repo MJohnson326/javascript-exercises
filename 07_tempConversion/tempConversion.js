46c74fd9eca4e420864e23e68bb1382cfc70d934
Melissa Johnson <melissajohnson326@gmail.com>
2023-01-26 19:44:31 -0500

    add comments

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
 const convertToCelsius = function(fahrenheit) {
-  let celsius = ((fahrenheit - 32) * (5/9))
-  return Math.round(celsius*10)/10
+  let celsius = ((fahrenheit - 32) * (5/9))// F to C math conversion
+  return Math.round(celsius*10)/10 //rounds to one decimal place
   
 };
 
 const convertToFahrenheit = function(celsius) {
-  let fahrenheit =((celsius * (9/5))+32)
-  return Math.round(fahrenheit *10)/10
+  let fahrenheit =((celsius * (9/5))+32)// C to F math conversion
+  return Math.round(fahrenheit *10)/10 // rounds to one decimal place
 };
 
 // Do not edit below this line
